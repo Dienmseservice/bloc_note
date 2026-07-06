995297722bffbce99e41aa73cc100f679e98de17
Dienmse services <dsmecom42@gmail.com>
2026-07-06 03:31:30 +0000

    Fix: Support SQLite pour le déploiement Web sur Vercel

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'views/welcome_view.dart';
 
 void main() {
+  // Garantit la bonne initialisation des liaisons Flutter
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Si l'application s'exécute sur le Web, on redirige SQLite vers l'adaptateur virtuel Web
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  }
+
   runApp(const EcoSaveNotesApp());
 }
 
